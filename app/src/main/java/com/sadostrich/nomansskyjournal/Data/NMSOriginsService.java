7d31deac55ea41f308e053f8a7ee8b3d6179f704
jacewardell <jace.wardell@icentris.com>
2016-08-07 20:28:26 -0600

    Updated the url properly

--- app/src/main/java/com/sadostrich/nomansskyjournal/Data/NMSOriginsService.java
+++ app/src/main/java/com/sadostrich/nomansskyjournal/Data/NMSOriginsService.java
@@ -17,7 +17,7 @@ import retrofit2.http.POST;
  */
 public interface NMSOriginsService {
 //	String BASE_URL = "https://www.nmsorigins.com/";
-	String BASE_URL = "https://www.dev.nmsorigins.com";
+	String BASE_URL = "https://dev.nmsorigins.com/";
 
 	/////////////////////////////////////////////////////////
 	// Request Placeholders
