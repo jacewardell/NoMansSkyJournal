bdab2ad78be50d8e45713e5de9fb4e9fa42d17d9
jacewardell <jace.wardell@icentris.com>
2016-08-07 20:12:34 -0600

    Updated to use dev environment

--- app/src/main/java/com/sadostrich/nomansskyjournal/Data/NMSOriginsService.java
+++ app/src/main/java/com/sadostrich/nomansskyjournal/Data/NMSOriginsService.java
@@ -16,7 +16,8 @@ import retrofit2.http.POST;
  * Created by jacewardell on 5/10/16.
  */
 public interface NMSOriginsService {
-	String BASE_URL = "https://www.nmsorigins.com/";
+//	String BASE_URL = "https://www.nmsorigins.com/";
+	String BASE_URL = "https://www.dev.nmsorigins.com";
 
 	/////////////////////////////////////////////////////////
 	// Request Placeholders
